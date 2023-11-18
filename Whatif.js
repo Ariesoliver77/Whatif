4b56aa3995f83a8f8df8a33ffe932a254c046597
Ariesoliver77 <145284374+Ariesoliver77@users.noreply.github.com>
2023-11-19 07:14:12 +0800

    Create Whatif.js

---
+++ Whatif.js
@@ -0,0 +1,17 @@
+alert("Create By AriesOliver");
+
+
+
+alert("What if napilitan lang talaga akong umiwas kaka overthink ko?");
+
+alert("what if gusto ko talaga sya pero sa iba ako nakinig?");
+
+alert("What if tinatawanan lang nila ako habang ka chat ko sya?");
+
+alert("what if di nya talaga ako gusto?");
+
+alert("What if may bago pala sya kaya sya nakipag hiwalay?");
+
+alert("what if umasa lang ako sa wala");
+alert("What if iba naman pala gusto nya?");
+alert("what if mahal moko as a friend?")
